f85a79874a2f7bc4d28b738e7c70e311a66a426f
300702 <jim@homm.es>
2025-12-09 20:30:48 +0100

    Day 1 part 1

--- 2025/Day1.py
+++ 2025/Day1.py
@@ -1,2 +1,36 @@
-print('hello')
+from aocd import *
 
+data_input = get_data(day=1, year=2025)
+# data_input = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82"
+dial = 50
+
+
+def dial_right(to_right):
+    new_dial = dial + int(to_right)
+    while new_dial > 99:
+        new_dial = new_dial - 100
+    return new_dial
+
+
+def dial_left(to_left):
+    new_dial = dial - int(to_left)
+    while new_dial < 0:
+        new_dial = new_dial + 100
+    return new_dial
+
+
+ex1_res = 0
+for line in data_input.split('\n'):
+    print(line)
+    if line[0] == 'L':
+        dial = dial_left(line[1:])
+    elif line[0] == 'R':
+        dial = dial_right(line[1:])
+
+    if dial == 0:
+        ex1_res += 1
+    print('Dial now at ' + str(dial))
+    print('Count now at ' + str(ex1_res))
+
+print(ex1_res)
+submit(ex1_res, part='a', day=1, year=2025)
