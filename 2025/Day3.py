4ab90244543dc793f3b08b19991f95e987a9abef
300702 <jim@homm.es>
2025-12-10 18:42:25 +0100

    Rewrote Day 3 Part A as recursive function

--- 2025/Day3.py
+++ 2025/Day3.py
@@ -5,17 +5,26 @@ data_input = get_data(day=3, year=2025)
 # data_input = '987654321111111\n811111111111119\n234234234234278\n818181911112111'
 
 
-def get_maximum_joltage(bank):
+def get_maximum_joltage(bank, battery_length):
+    return int(get_maximum_joltage_rec(bank, battery_length, 1))
+
+
+def get_maximum_joltage_rec(current_bank, battery_length, current_depth):
     for i in reversed(range(10)):
-        for j in reversed(range(10)):
-            if str(i) in bank and str(j) in bank[bank.index(str(i))+1:]:
-                return int(str(i) + str(j))
+        if str(i) in current_bank[0:len(current_bank)-(battery_length-current_depth)]:
+            if battery_length == current_depth:
+                return str(i)
+            else:
+                rec_res = get_maximum_joltage_rec(current_bank[current_bank.index(str(i))+1:], battery_length, current_depth + 1)
+                if rec_res is not None:
+                    return str(i) + rec_res
+    return None
 
 
 t1_start = perf_counter()
 ex1_res = 0
 for line in data_input.split('\n'):
-    ex1_res += get_maximum_joltage(line)
+    ex1_res += get_maximum_joltage(line, 2)
 t1_stop = perf_counter()
 print('Part A - Answer: ' + str(ex1_res) + ', calculated in ' + str((t1_stop - t1_start) * 1000) + ' ms')
 submit(ex1_res, part='a', day=3, year=2025)
